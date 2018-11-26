3b62e97dd3ee0f83a00ec45870f358f408aa0217
Lenin Martinez <lenin.martinez123@gmail.com>
2018-11-26 15:19:20 -0300

    deprecating preSigned feature

--- Sources/App/setup/routes.swift
+++ Sources/App/setup/routes.swift
@@ -3,5 +3,4 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     try router.register(collection: FilesController())
-    try router.register(collection: SignController())
 }
